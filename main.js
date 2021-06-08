0dbd75b136385c45db450f4920fd38551b2203a3
alejandrojosue <67884952+alejandrojosue@users.noreply.github.com>
2021-06-08 12:15:53 -0600

    Update main.js

--- main.js
+++ main.js
@@ -93,6 +93,7 @@ if (getMobileOperatingSystem() != 'desconocido') {
     });
     
     if ('serviceWorker' in navigator) {
+        alert('seccion de service worker existente');
         navigator.serviceWorker.register('./sw.js')
             .then(reg => console.log('Registro de SW exitoso', reg))
             .catch(err => console.warn('Error al tratar de registrar el sw', err))
